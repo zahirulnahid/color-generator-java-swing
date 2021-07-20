c9305638816182320034253251be6e0fda5b7650
zahirulnahid <zahirulnahid@gmail.com>
2021-07-20 13:29:00 +0600

    20Jul2021

---
+++ src/colorgenarate/ColorGenerate.java
@@ -0,0 +1,17 @@
+
+package colorgenarate;
+
+/**
+ *
+ * @author zahirul
+ */
+public class ColorGenerate {
+
+
+    public static void main(String[] args) {
+       ui ui = new ui();
+       ui.setVisible(true);
+       ui.setTitle("Color Generator");
+    }
+    
+}
